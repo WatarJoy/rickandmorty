78c67eb5551e893a248cd267e926768141235f91
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2025-04-09 01:52:21 +0300

    path fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/rick_and_morty',
+  basePath: '/rickandmorty',
   images: {
     domains: ["rickandmortyapi.com"],
   },
